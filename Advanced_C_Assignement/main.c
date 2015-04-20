c1b637c7c4fe24796e1e0b07d87b0b9d45d78202
John Malcolm <hi@johnmalcolmdesign.com>
2015-04-20 16:25:35 +0100

    Initial Commit

---
+++ Advanced_C_Assignement/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Advanced_C_Assignement
+//
+//  Created by John Malcolm Anderson on 20/04/2015.
+//  Copyright (c) 2015 John Malcolm Anderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
